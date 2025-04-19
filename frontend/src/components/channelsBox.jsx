82dfbdceac7e2698d9b63b618328fd220709bcbe
shelser <shelserg@gmail.com>
2025-04-19 09:01:02 +0300

    changed proxy port

--- frontend/src/components/channelsBox.jsx
+++ frontend/src/components/channelsBox.jsx
@@ -9,7 +9,7 @@ import io from 'socket.io-client';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer, toast } from 'react-toastify';
 
-const socket = io('ws://localhost:5002');
+const socket = io('ws://localhost:5001');
 
 
 const ChannelsBox = () => {
